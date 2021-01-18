a5fde668efa555e1b4ed457ef21468d6c65f8485
garesuta <garesuta@gmail.com>
2021-01-18 22:18:21 +0700

    Add recycler for Pizza fragment

--- app/src/main/java/com/example/bitsandpizzas/PizzaFragment.java
+++ app/src/main/java/com/example/bitsandpizzas/PizzaFragment.java
@@ -1,22 +1,30 @@
 package com.example.bitsandpizzas;
 
 import android.os.Bundle;
-import androidx.fragment.app.ListFragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import androidx.recyclerview.widget.RecyclerView;
 
-public class PizzaFragment extends ListFragment {
+
+public class PizzaFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pizzas));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+     RecyclerView pizzaRecycler = (RecyclerView)inflater.inflate(R.layout.fragment_pizza,container,false);
+     String[] pizzaNames = new String[Pizza.pizzas.length];
+     for (int i = 0;i < pizzaNames.length; i++){
+         pizzaNames[i]= Pizza.pizzas[i].getName();
+     }
+     int[] pizzaImages = new int[Pizza.pizzas.length];
+     for(int i=0;i<pizzaImages.length;i++){
+         pizzaImages = Pizza.pizzas[i].getImageResourceId();
+     }
+     CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pizzaNames,pizzaImages);
+     pizzaRecycler.setAdapter(adapter);
+     return pizzaRecycler;
     }
 }
